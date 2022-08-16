5aa528eed868e5f29a51d0071f231df64d7d5df9
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-16 19:35:54 +0200

    Update level05_lemma_usage.lean

--- src/tutorial_world/level05_lemma_usage.lean
+++ src/tutorial_world/level05_lemma_usage.lean
@@ -2,15 +2,16 @@ import tutorial_world.incidenceplane --hide
 open IncidencePlane --hide
 
 /- Axiom :
-line_through_left : P ∈ (line_through P Q)
+line_through_left (P Q : Ω) : P ∈ (line_through P Q)
 -/
 /- Axiom :
-line_through_right : Q ∈ (line_through P Q)
+line_through_right (P Q : Ω) : Q ∈ (line_through P Q)
 -/
 
 /-
-We can apply a theorem that we have already proven by using `exact`
-and the appropriate hypotheses.
+# Tutorial World
+
+## The first axiom of incidence.
 -/
 
 variables {Ω : Type} [IncidencePlane Ω] --hide
@@ -21,8 +22,8 @@ A point lies in the line through it.
 lemma point_on_line {A B : Ω} {r : Line Ω} :
 B ∈ line_through A B :=
 begin
+  
   exact line_through_right A B,
-
 
 end
 
