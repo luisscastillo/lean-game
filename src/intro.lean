4a11b7a271401d8475707ff9bfa35e9e75a5d81b
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-16 11:41:24 +0200

    Update intro.lean

--- src/intro.lean
+++ src/intro.lean
@@ -1,18 +1,40 @@
 /-
 # The Euclid Game, version 1.0.0.
 
-Where the past and the future of mathematics meet!
+## Where the past and the future of mathematics meet!
 
 # What is this game?
 
-Welcome to The Euclid Game! In this game, you will learn how to teach mathematics to the computer by 
-using some facts about geometry. 
+Welcome to The Euclid Game! In this game, you will learn the basics of how a program called a *proof assistant* 
+works and how it is used to formalize some mathematical results.
+What does this mean? Instead of writing down proofs in English or any other human language, we will
+explain the steps of the proof to the proof assistant by using a precise language, similar to a
+programming language in some aspects.
+The advantage of doing this is that the computer can keep track of the proof for us, and tell
+us if we make any steps that aren't logically valid; computers are very good at understanding logic
+after all!
+Other than needing to use specific commands to explain the proof steps, we can work on proofs like
+usual, using definitions and already established facts to show new results.
+The proof assistant will completely keep track of what facts we know so far, and show us what else
+needs to be shown to finish the current proof, importantly it won't let us show something that
+doesn't logically follow from what we have proved so far.
 
+This is a very literal sort of computer assisted proof, we are using a computer to step through and
+understand individual steps in a proof.
+Later we will see how the assistant can do more than just follow our instructions; it can also
+search for useful facts we might want to use. Lean can even complete some moderately
+difficult statements on its own, using proof methods based on pre-defined rules.
+This saves us time thinking about details in some situations, without losing any confidence in the
+correctness of the result, and in some cases we can even learn from the proofs Lean finds!
+To get started, click on the first world, the blue circle labelled Tutorial World, on the right.
+Once you complete all the levels of the Tutorial World, the blue circle will turn into green, which 
+will give you access to the following world.
+You can use this menu to navigate as you try more of the problems.
 Have fun!
 
 # Thanks
 
-The BIYSC team on "Can computers do math?" at UAB.
+The Euclid Game was created as part of the BIYSC program on "Can computers do math?" at Universitat Autònoma de Barcelona.
 
 Some of the tactic descriptions are taken/adapted from the original Natural Number Game (https://www.ma.imperial.ac.uk/~buzzard/xena/natural_number_game/).
 
