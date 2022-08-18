5044c4907e0ac048ca39c0d7cab43f1d812d0a8f
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-18 12:31:06 +0200

    Update level05_lemma_usage.lean

--- src/tutorial_world/level05_lemma_usage.lean
+++ src/tutorial_world/level05_lemma_usage.lean
@@ -23,19 +23,22 @@ World"** box to discover what we are talking about. As you can see, two new stat
 
 **Note the name of the two statements**. Mathematicians sometimes call them "Lemma 2.1" or "Hypothesis P6" or something. But
 computer scientists call them `line_through_left` and `line_through_right` because they are easier to use and remember. From now on,
-all the statements that appear on this list will be remembered by the computer. In this way, we won't have to provide their proof again.
+all the statements that appear on this list will be remembered by the computer. In this way, we won't have to provide their proofs again.
 Instead of this, we will use them straightforwardly in case they are handy for solving the following levels. 
 
 Just after the name of the statements, two parentheses appear. Inside them, there are the exact number of variables that are needed to
-put out that statements. Then, the colon **:** symbol introduces the statement as such. 
+put out that statements. Then, the colon **:** symbol introduces the statement as such. In this case, they are very similar to each other.
+What they come to mean is that we can draw a line that passes through two distinct points (P and Q) that lie in a plane (Ω). They symbol **∈**
+is read as **"is an element of"**. Then, P and Q are elements of the line that passes through the points P and Q. Seems obvious, right? Now, let's 
+try to solve this level! 
 
+Delete the `sorry` and watch the goal appear as `⊢ B ∈ line_through A B`. Presumably, the goal shows the same structure as
+the statement `line_through_right (P Q : Point) : Q ∈ (line_through P Q)`. Then, we just have to write that statement in a different way! 
+-- How do we change the points P and Q for the points A and B? -- Follow the same structure of the statement we need to use. Do you remember that the `exact` 
+tactic solved the goal by using a hypothesis of the same structure? Then, because the computer already knows what
+`line_through_right (P Q : Point) : Q ∈ (line_through P Q)` means, why don't we type `exact line_through_right A B,`? Type that and see 
+how it finishes the proof! [**Don't forget to write the variables A and B. They are needed to use the statement**.]
 
-Delete the `sorry` and see that the goal is ⊢ B ∈ line_through A B. Presumably, the goal shows the same structure as
-the statement `line_through_right (P Q : Point) : Q ∈ (line_through P Q)`. [Remember that the points P and Q could be needles!]. Then, we just 
-have to write that statement in a different way! -- How do we change the points P and Q for A and B? -- Follow the same structure of the statement
-we need to use. Do you remember that the `exact` tactic solved the goal by using a hypothesis of the same structure? Then, because the computer
-already knows what `line_through_right (P Q : Point) : Q ∈ (line_through P Q)` means, why don't we type `exact line_through_right A B,`? Type that and see 
-how it finishes the proof! 
 -/
 
 variables {Ω : Type} [IncidencePlane Ω] --hide
@@ -50,6 +53,4 @@ begin
   exact line_through_right A B,
 
 end
-
-
 
