cd9148e0f6b44c12b851e54721b87034d21241cd
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-18 15:40:04 +0200

    Update level09_have.lean

--- src/tutorial_world/level09_have.lean
+++ src/tutorial_world/level09_have.lean
@@ -49,11 +49,42 @@ and your original goal will have `q : Q` added to the list of hypotheses.
 -/
 
 /-
-In this level we introduce the new tactic `have`. It is used to add a new hypothesis
-to the context (of course, you will have to prove it!). This is sometimes useful to
-structure our proofs. In this particular level, it is convenient to prove first that
-`r = line_through B C`, then that `s = line_through B C` and that allows us to
-finish the prove very easily.
+# Tutorial World 
+
+## Level 9: The `have` tactic.
+
+Congratulations! You are half of the way to finish this world! In this level, we introduce the new tactic `have`. It is used to add a new hypothesis
+to the context (which, of course, you will have to prove!). This is sometimes useful to structure our proofs. In this particular level, it is convenient
+to prove first that `r = line_through B C`, and then that `s = line_through B C`. This strategy will allows us to finish the prove very easily!
+
+To use the tactic `have`, we should follow the following structure: `have h : A = B,`. This line will add the hypothesis `h : A = B` to the local
+context and break the proof into two goals. First, Lean will ask us to prove `⊢ A = B` without the hypothesis `h : A = B`. Then, it will ask us to
+prove the goal that existed before using the tactic `have` with the support of the new hypothesis `h : A = B` that we have added to the local context.
+
+**Pro tip:** Because you're getting better at this, proofs are going to be more challenging as time goes by. Whenever you see that you have to prove two 
+or more goals to finsih one level, you may want to use **curly braces**; that is to say, the **{** - **}** symbols. Inside each of them, you just have to
+prove one goal. Then, whenever you want to prove the following one, just open curly braces again. **Don't forget to close them with a comma at the end!**
+
+For example, the first line of this proof will be `have hr : r = line_through B C,` (you can change `hr` into whatever name you are comfortable with to
+make reference to the hypothesis `r = line_through B C`). Now, because two goals have appeared, you can type the following structure: 
+
+{
+  
+  sorry,
+  
+}, 
+
+{
+
+  sorry, 
+  
+}, 
+
+In this way, by deleting the `sorry`'s, you will be able to prove the goals separately. Now, let's try solve this level together so that you can
+easily understand how the syntax of Lean works! 
+
+First, we are going to prove the first goal, which is `⊢ r = line_through B C,`. To begin with, let's look at the "theorem statements" we have. 
+
 -/
 
 variables {Ω : Type} [IncidencePlane Ω] --hide
