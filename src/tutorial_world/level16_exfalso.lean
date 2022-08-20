48be747d838917eed9abad426add1a45a388372e
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-20 16:27:20 +0200

    Update level16_exfalso.lean

--- src/tutorial_world/level16_exfalso.lean
+++ src/tutorial_world/level16_exfalso.lean
@@ -22,15 +22,21 @@ we can then `apply h` to transform the goal into `⊢ P`.
 
 ## Level 16: the `exfalso` tactic. 
 
-In this level we introduce the new tactic `exfalso`. Look at what it does, it is a bit
-strange at first. We will also need one of the axioms for our plane, the one that says that
-the line through two points contains each of them. You can see the statement of this theorem
-on the left sidebar.
+In this level we introduce the new tactic `exfalso`. It satifies the **Principle of explosion** of classical logic, 
+according to which any statement can be proven from a contradiction. In Lean, if we type `exfalso`, the goal will turn
+into `⊢ false`. Let's solve this level to see how it works! 
+
+Delete the `sorry` and take a look at the hypothesis `h`, according to which the point P is not an element of the line 
+that passes through the points P and Q. This is a contradiction that can be rewritten as `¬ (P ∈ line_through P Q)`, where 
+the symbol **¬** means "not". Moreover, it can also be rewritten as `P ∈ line_through P Q → false`. This last way of representing
+the contradiction is key to complete this level. By typing `exfalso`, we know that the goal will change into `⊢ false`. Then, look
+for a tactic that can turn the goal into `⊢ P ∈ line_through P Q` and you will be almost done! In case you get stuck, click right below for a hint.
 -/
 
 /- Hint : Click here for a hint, in case you get stuck.
-In Lean, the  negation `¬ P` of a statement is a shorthand for `P → false`. Therefore
-start with `exfalso`, and remember that negation is the same as `→ false`.
+The `apply` tactic changes the goal from `⊢ Q` to `⊢ P` when we have a hypothesis of the form `h : P → Q`. In this case, `h : P ∉ line_through P Q`
+can be interpreted as `P ∈ line_through P Q → false`. Then, because the goal is `⊢ false', `apply h,` will make progress. Still bewildered? Click on "View source" 
+(located on the top right corner of the game screen) to see the solution. 
 -/
 
 variables {Ω : Type} [IncidencePlane Ω] --hide
@@ -40,13 +46,9 @@ Prove that 2+2 is 5, using a false hypothesis.
 -/
 lemma two_plus_two_equals_five (P Q : Ω) (h: P ∉ line_through P Q) : 2 + 2 = 5:=
 begin
+
   exfalso,
   apply h,
   exact line_through_left P Q,
-
-
-
-
-
   
 end 
