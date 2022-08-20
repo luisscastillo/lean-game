0d20d075038ea7ed57bef363283592a4e061032e
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-20 14:16:22 +0200

    Update level15_simp.lean

--- src/tutorial_world/level15_simp.lean
+++ src/tutorial_world/level15_simp.lean
@@ -25,12 +25,12 @@ the right-hand side, which is more concrete.
 In this level, we introduce a high level tactic called `simp`. This is an Artificial Intelligence (AI) tactic which 
 can nail some really tedious-for-a-human-to-solve goals. It uses lemmas that are already in our database to make 
 the goal simpler for us to solve. 
-
-Now, read the lemma and do a drawing of the situation. In this level, we also introduce a geometric concept which may be useful to remember. **A segment 
-As a simple example, for `S` being a segment, we have a lemma
-`mem_pts : P ∈ S ↔ P = S.A ∨ P = S.B ∨ (S.A * P * S.B)`, and it is tagged as a simp lemma
+ 
+As a simple example, for `S` being a segment (the concept of segment will be defined in the Betweenness World), we have the lemma
+`mem_pts : P ∈ S ↔ P = S.A ∨ P = S.B ∨ (S.A * P * S.B)`, which is tagged as a simp lemma
 in this game. This means that the `simp` tactic will replace occurrences of `P ∈ S` with
-the right-hand side, which is more concrete. Try it below.
+the right-hand side of the double implication, which is more concrete. Now, delete the `sorry` and type
+`simp,`. After that, try to complete the level by using the tactics that we've learned so far. 
 -/
 
 variables {Ω : Type} [IncidencePlane Ω] --hide
@@ -40,13 +40,11 @@ A point in between the endpoints of a segment is in the segment.
 -/
 lemma simp_example (P : Ω) (S : Segment Ω) (h : S.A * P * S.B) : P ∈ S :=
 begin
+
   simp,
   right,
   right,
   exact h,
-
-
-
 
 end
 
