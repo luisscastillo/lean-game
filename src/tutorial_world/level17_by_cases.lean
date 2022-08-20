9a5ea40aed654e530f49becd4bc10514926eea35
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-20 17:23:06 +0200

    Update level17_by_cases.lean

--- src/tutorial_world/level17_by_cases.lean
+++ src/tutorial_world/level17_by_cases.lean
@@ -19,9 +19,23 @@ and the second one with `h : ¬ x ≤ 5`.
 -/
 
 /-
-Sometimes we need to split a proof into different cases,
-because the arguments depend on some condition. The `by_cases`
-tactic does precisely this.
+# Tutorial World 
+
+## Level 17: the `by_cases` tactic.
+
+In this level, we introduce the `by_cases` tactic. This is useful when we need to split a proof into different cases, 
+For example, if we are asked to solve a goal of the form `⊢ P ∨ ¬ P`, then `by_cases h : P` will split the goal into two cases, 
+assuming `h : P` in the first branch, and `h : ¬ P` in the second branch. With that being said, let's try to solve this level!
+
+[**Tip:** You may want to write the `∈` symbol to solve this level. To do so, type **`\in`** and then hit the space bar. Analogously, 
+you can write the `∉` symbol by typing **`\notin`** and then hitting the space bar.]
+
+-/
+
+/- Hint : Click here for a hint, in case you get stuck.
+Starting the proof by typing `by_cases h : A ∈ r` will split the goal into two cases, assuming `h : A ∈ r` in the first branch, 
+and h : A ∉ r` in the second branch. Then, try to look for a tactic that changes the goal from `⊢ P ∨ Q` to either `⊢ P` or `⊢ Q`.
+Still bewildered? Click on "View source" (located on the top right corner of the game screen) to see the solution. 
 -/
 
 variables {Ω : Type} [IncidencePlane Ω] --hide
@@ -31,6 +45,7 @@ Either a point is in a line or it is not.
 -/
 lemma point_in_line_or_not {A : Ω}	{r : Line Ω} : A ∈ r ∨ A ∉ r :=
 begin
+
   by_cases h : A ∈ r,
   { 
     left,
@@ -39,12 +54,6 @@ begin
   { 
     right,
     exact h,
-  }
-
-
-  
-
-
-
+  },
   
 end
