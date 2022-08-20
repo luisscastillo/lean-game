d25194056cd2fbd0e475ddfb861e6831624010e3
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-20 16:53:52 +0200

    Update intro.lean

--- src/intro.lean
+++ src/intro.lean
@@ -13,11 +13,6 @@ programming language in some aspects.
 The advantage of doing this is that the computer can keep track of the proof for us, and tell
 us if we make any steps that aren't logically valid; computers are very good at understanding logic
 after all!
-Other than needing to use specific commands to explain the proof steps, we can work on proofs like
-usual, using definitions and already established facts to show new results.
-The proof assistant will completely keep track of what facts we know so far, and show us what else
-needs to be shown to finish the current proof, importantly it won't let us show something that
-doesn't logically follow from what we have proved so far.
 
 What makes *proof assistants* unique is the fact that they allow us to step through and
 understand individual steps in a proof. A mathematician would remark that we are providing **computer-assisted proofs**. 
@@ -27,8 +22,8 @@ difficult statements on its own, using proof methods based on pre-defined rules.
 This saves us time thinking about details in some situations, without losing any confidence in the
 correctness of the result, and in some cases we can even learn from the proofs Lean finds!
 
-To get started, click on the first world, the blue circle labelled Tutorial World, on the right.
-Once you complete all the levels of the Tutorial World, hit "Main Menu" in the top left to get back here. 
+To get started, click on the first world, the blue circle labelled "Tutorial World", on the right.
+Once you complete all the levels of the Tutorial World, hit "Main Menu" in the top left corner of the screen to get back here. 
 In this way, you will see that the first blue circle has turned into green, which 
 will give you access to the following world.
 You can use this menu to navigate as you try more of the problems.
