9f595ad2d21ab2d09a206df3cd4eef585e66f5b4
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-20 14:04:26 +0200

    Update level15_simp.lean

--- src/tutorial_world/level15_simp.lean
+++ src/tutorial_world/level15_simp.lean
@@ -24,7 +24,7 @@ the right-hand side, which is more concrete.
 
 In this level, we introduce a high level tactic called `simp`. This is an Artificial Intelligence (AI) tactic which 
 can nail some really tedious-for-a-human-to-solve goals. It uses lemmas that are already in our database to make 
-the goal easier to solve. 
+the goal simpler for us to solve. 
 
 Now, read the lemma and do a drawing of the situation. In this level, we also introduce a geometric concept which may be useful to remember. **A segment 
 As a simple example, for `S` being a segment, we have a lemma
