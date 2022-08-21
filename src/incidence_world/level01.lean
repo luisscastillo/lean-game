9cf76e6c7cabf728c8755da5a493c23e87360b64
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-21 15:18:37 +0200

    Update level01.lean

--- src/incidence_world/level01.lean
+++ src/incidence_world/level01.lean
@@ -1,8 +1,8 @@
-import tutorial_world.level18_practice --hide
+import tutorial_world.level18_by_contra --hide
 open IncidencePlane --hide
 
 /- Axiom :
-existence : ∃ P Q R : Ω, P ≠ Q ∧ P ≠ R ∧ Q ≠ R ∧ R ∉ (line_through P Q)
+existence (Ω : Type) : ∃ P Q R : Ω, P ≠ Q ∧ P ≠ R ∧ Q ≠ R ∧ R ∉ (line_through P Q)
 -/
 
 /-
@@ -19,10 +19,10 @@ They are served as a premise for further reasoning and arguments, so that we can
 By travelling back in time to 300 B.C., we meet the great mathematician Euclid, who suggested the very 
 first postulates of geometry in his well-known book **`Elements`**. Euclidean geometry can be built up from three
 separate sets of axioms, each of them adding new independent notions that are needed to define the plane. These sets of axioms 
-were proposed by David Hilbert (1862-1943 AD), who made remarkable improvements in the Foundations of geometry.
+were proposed by David Hilbert (1862-1943 AD), who made remarkable improvements in the foundations of geometry.
 These three sets are called **incidence**, **betweenness** and **congruence** (we might also want to add the **Parallel Axiom**).
 
-Inside the first set of axioms, there are up to three axioms of incidence. These are established to define
+When it comes to the first set of axioms, there are up to three axioms of incidence. These are established to define
 the primitive notions of **point**, **line** and the relationship between these two concepts, which is called **incidence**. Notice 
 that by "incidence" we mean whatever idea that satifies the axioms of incidence. Then, you will be wondering... are the
 notions of "point" and "line" referring to whatever object of reality that satisfies the axioms of incidence? Exactly!
@@ -56,13 +56,58 @@ How do we make the computer understand such complex statements? By using Type Th
 some of them are such difficult for a computer to comprehend that they must be divided into more than one statement. For example, the first axiom is 
 divided into four statements: 
 
-(line_through' : Point → Point → Line)
+line_through (P Q : Ω) : Line Ω := line_through' P Q
 
-(line_through_left' (P Q : Point) : P ∈ (line_through' P Q))
+line_through_left (P Q : Ω) : P ∈ (line_through P Q)
 
-(line_through_right' (P Q : Point) : Q ∈ (line_through' P Q))
+line_through_right (P Q : Ω) : Q ∈ (line_through P Q)
 
-(incidence' {P Q : Point} {ℓ : Line} : P ≠ Q → P ∈ ℓ → Q ∈ ℓ → ℓ = line_through' P Q)
+incidence {P Q : Ω} {ℓ : Line Ω} : P ≠ Q → P ∈ ℓ → Q ∈ ℓ → ℓ = line_through P Q
+
+Here it comes the second axiom of incidence in Lean: 
+
+line_contains_two_points (ℓ : Line Ω) : ∃ P Q : Ω, P ≠ Q ∧ ℓ = line_through P Q
+
+And, to finish with, the third one appears right below: 
+
+existence (Ω : Type) : ∃ P Q R : Ω, P ≠ Q ∧ P ≠ R ∧ Q ≠ R ∧ R ∉ (line_through P Q)
+
+With that being said, let's try to solve the first level of this world together! 
+
+## Let's solve this level together! 
+
+Now that we have learned the basic Lean tactics, we are ready to prove our first theorems! 
+
+The goal of this world is to prove the existence of triangles, but we will start showing that there is no line 
+that covers the whole plane. That is to say, every line misses at least one point.
+
+To solve this level, we will need to use the third axiom of incidence. For this reason, the theorem statement 
+called `existence` has been added to the list of our theorem statements (located at the left-hand side of the game screen).
+[**Remember:** Despite being in the Incidence World, we can freely use the theorem statements from the Tutorial World.]
+
+To begin with, we are going to understand why we need to start our proof by using the third axiom of incidence. Read the lemma 
+of this level and do a drawing of the situation. Can you see that there is a point which is not in the line you have drawed? Now, go above and 
+read the first two axioms of incidence. Then, come back here. Have you noticed it? All the points which are considered in the first two axioms 
+pass through a line! Meanwhile, if we want to prove our lemma, we need to generate a point which is not in a line! Now, go above and read the 
+third axiom of incidence. Because this is the only axiom that considers a point which is not in a line, we must start by using it!
+
+Once we've discovered how to start our proof, let's now define the objects that we are going to use! First, we need three distinct points which 
+will be called A, B and C. Then, we need a plane Ω that contains all of them! Now, you may be wondering if we have to define a line as well...
+The answer is that we will have to define more than one line in fact, but we won't do this at the beginning of the proof because it's not necessary yet. 
+However, you might be asking... why can we not define a line that passes through two of the three points that we have defined and say that the line misses 
+one of the points? The answer is simple: that is not what we want. If you read the lemma again, you will see that it considers **every** possible line of the plane. 
+Then, if we define a line, we are just proving one case out of all the lines that could exist in a plane!
+
+Because of this reason, **we must divide our proof into different cases**. That is, into different possible lines. Does this mean that we have to consider as many
+cases as lines exist in a plane? Absolutely not! If that was the case, we would need infinite lifes to finish that proof! What we need to do, instead, is to
+**provide as less cases as possible to prove that the lemma is true**. Then, **how do we know the minimum number of cases that are needed to prove this lemma?** 
+The answer to this question is **by knowing the minimum number of dimensions that are needed to satisfy the lemma**. If you try to prove this lemma in non-dimensional
+or one-dimensional spaces, you will see that it is not possible. However, in two-dimensional spaces (that is, the plane) it **is** possible to prove it. And because
+the minimum number of points to build the plane is three, then we can prove this lemma just by generating three points. 
+
+Now that we have divided the proof into 
+
+
 -/
 
 /- Hint : Click here for a hint, in case you get stuck.
