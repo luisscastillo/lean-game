9a73e0eea08b44241a89682478826ef8340c68e1
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-22 12:48:20 +0200

    Update level03.lean

--- src/incidence_world/level03.lean
+++ src/incidence_world/level03.lean
@@ -2,6 +2,32 @@ import incidence_world.level02--hide
 open IncidencePlane --hide
 
 /-
+## Incidence World
+
+# Level 3: in search of the extreme cases. 
+
+In axiomatic geometry, when we are given a point, we can't determine where it is. Then, if we need to
+generate more points to prove a lemma, it might be the case where one of these points drops exactly where 
+the given point was located. Analogously, it might be the case where two of the points generated make a line with 
+the given point, or perhaps they don't do so... Because of this reason, we should think about all the possible
+distributions that are needed to satisfy a lemma. Let's come up with them by solving the level! 
+
+To begin with, read the lemma and do a drawing of the situation. To solve this level, we are given a point; call it P. 
+Now, try to think of an axiom to set up our proof. If you are not sure about which one to choose, let's step through them one by one. 
+It might be the case where some of them cannot be used, thus reducing the number of usable axioms and hence increasing the probability 
+of using the ideal one. 
+
+For example, let's say that we want to start our proof with the first axiom of incidence. In this case, we would generate two 
+points (A and B) to make a line that passes through them. In case we are lucky, these points won't be dropped exactly where the given point P is spotted.
+Then, we would be done because the points A, B and P are not collinear. However, it might be also the case where these three points happened to pass through
+the same line. Then, the points A, B and P would be collinear, and this is not what we want. For this reason, the first axiom of incidence is not a good candidate
+to set up our proof.
+
+Following the above train of thought, you wil get to the point where the third axiom of incidence is the correct one to choose. Still and all, 
+we have to think of all the possible distributions that could arise when having three non-collinear points (A, B and C) and the given point P. 
+It might be the case where the point P is not spo
+
+
 Using the lemma `point_in_line_not_point` that you proved in the previous
 level, we can prove a stronger version of the existence axiom. Remember that
 this axiom says that there are three distinct non-collinear points. The
