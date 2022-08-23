7bcd3e5bcc93f4be2c666ba1e1fc1bcbdde8d470
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-23 23:20:40 +0200

    Update level04.lean

--- src/betweenness_world/level04.lean
+++ src/betweenness_world/level04.lean
@@ -37,3 +37,25 @@ variables {ℓ r s t : Line Ω} --hide
 /- Lemma :
 A point that lies between two different collinear points shares the same line with them.
 -/
+lemma between_points_share_line_v2 (hAr : A ∈ r) (hBr : B ∈ r) : 
+	(A * B * C) → C ∈ r :=
+begin
+    intro H,
+    have h1 : A ≠ B ∧ A ≠ C ∧ B ≠ C,
+    {
+        exact different_of_between H,
+    },
+    have h2 : ∃ ℓ, A ∈ ℓ ∧ B ∈ ℓ ∧ C ∈ ℓ,
+    { 
+        apply collinear_of_between,
+        exact H,
+    },
+    cases h2 with s hs,
+    have h3 : r = s,
+    {
+        exact equal_lines_of_contain_two_points h1.1 hAr hs.1 hBr hs.2.1,
+    },
+    rw h3,
+    exact hs.2.2,
+	
+end
