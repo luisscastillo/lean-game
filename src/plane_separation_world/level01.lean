cf4f04ef7f84a9e7bb0a8470c3f78954c2bd0546
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-23 11:51:28 +0200

    Update level01.lean

--- src/plane_separation_world/level01.lean
+++ src/plane_separation_world/level01.lean
@@ -16,47 +16,107 @@ corner of the game screen) to see the solution.
 -/
 
 variables {Ω : Type} [IncidencePlane Ω] --hide
+variables {A B C P Q R : Ω} --hide
+variables {ℓ r s t : Line Ω} --hide
 
 /- Lemma :
-There exist three lines that do not have a point in common.
+There...
 -/
-lemma three_distinct_lines : ∃ (r s t: Line Ω), (∀ (P : Ω),
-¬(P ∈ r ∧ P ∈ s ∧ P ∈ t)) :=
+lemma same_side_trans_of_noncollinear (hCol : ¬ collinear ({A, C, B} : set Ω)):
+same_side ℓ A B → same_side ℓ B C → same_side ℓ A C :=
 begin
-  
-  rcases existence Ω with ⟨A, B, C, ⟨hAB, hAC, hBC, h⟩⟩,
-  use line_through A B,
-  use line_through A C,
-  use line_through B C,
-  intros P H,
-  have h1 : line_through A C ≠ line_through A B, 
+  have hAC : A ≠ C,
   {
-    exact ne_of_not_share_point (line_through_right A C) h,
+    intro h,
+    apply hCol,
+    rw h,
+    use line_through B C,
+    simp, 
   },
-  by_cases hPA : P = A,
+-- Suppose l meets A·C (proving by contradiction)
+-- By Pasch l meets A·B or l meets B·C
+    -- If l meeets A·B then A, B on different sides
+    -- IF l meets B·C then B, C on different sides 
+  intros p q,
+  by_contradiction,
+  unfold same_side at h,
+  have h' : ∃ D , D ∈ pts (A⬝C) ∧ D ∈ ℓ,
   {
-    have hAlBC : A ∈ line_through B C,
+    simp,
+    by_contra h',
+    push_neg at h',
+    apply h,
+    ext,
+    split,
     {
-      rw ← hPA,
-      exact H.2.2,
+      intro h'',
+      simp,
+      specialize h' x,
+      have h''' : A*x*C,
+      {
+        finish,
+      },
+      apply h',
+      {
+        tauto,
+      },
+      {
+        finish,
+      },  
     },
-    have H1 : line_through A C = line_through B C,
     {
-      exact equal_lines_of_contain_two_points hAC (line_through_left A C) hAlBC (line_through_right A C) (line_through_right B C),
+      tauto,
     },
-    have H2 : line_through A C = line_through A B, 
-    {
-      rw H1,
-      exact equal_lines_of_contain_two_points hAB hAlBC (line_through_left A B) (line_through_left B C) (line_through_right A B),
-    },
-    exact h1 H2,
-  },
+},
+
+cases h' with P r,
+have c : A ∉ ℓ ∧ B ∉ ℓ ∧ C ∉ ℓ,
+{
+  repeat {split},
+  apply not_in_line_of_same_side_left p,
+  apply not_in_line_of_same_side_right p,
+  apply not_in_line_of_same_side_right q,
+},
+simp at r,
+have x : A*P*C,
+{
+  have hPA : P ≠ A,
   {
-    have h2 : line_through A C = line_through A B, 
-    {
-      exact equal_lines_of_contain_two_points hPA H.2.1 H.1 (line_through_left A C) (line_through_left A B),
-    },
-    tauto,
+    intro hc,
+    apply c.1,
+    rw hc at r,
+    exact r.2,
   },
-  
+  have hPA : P ≠ C,
+  {
+    intro hcon,
+    apply c.2.2,
+    rw hcon at r,
+    exact r.2,
+  },
+  tauto,
+},
+have a1 : B ∉ line_through A C,
+{
+  rw ← collinear_iff_on_line_through hAC,
+  exact hCol,
+},
+
+have fact := pasch a1 c.1 c.2.2 c.2.1 r.2 x,
+cases fact,
+{
+unfold same_side at p,
+cases fact.1 with E,
+simp [set.eq_empty_iff_forall_not_mem] at p,
+replace p := p.2.2 E h_1.2,
+apply p,
+exact h_1.1,
+},
+unfold same_side at q,
+cases fact.1 with F,
+simp [set.eq_empty_iff_forall_not_mem] at q,
+rw between_symmetric at h_1,
+replace q := q.2.2 F h_1.2,
+apply q,
+exact h_1.1,
 end
