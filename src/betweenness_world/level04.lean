bc0c989deb0175453dab947cd800821757aba828
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-24 07:51:49 +0200

    Update level04.lean

--- src/betweenness_world/level04.lean
+++ src/betweenness_world/level04.lean
@@ -7,26 +7,28 @@ open IncidencePlane --hide
 
 ## Level 4: another version of the previous level...
 
-To solve this level, the mathematical proof in paper will be given to you. Remember that you can use theorem statements from previous worlds.
+To solve this level, the mathematical proof in paper will be given to you **inside the Hint Box** (right below). 
+Remember that you can use theorem statements from previous worlds. Make an effort to write the proof by your own. 
+You may find the proof of the previous level very helpful for this one.
 
-**Claim:** A point that lies between two different collinear points shares the same line with them.
-
-**Proof:** Let B be the point that lies between A and B, where these two are different collinear points that lie on the line `r`.
-**(i)** Let us assume that there exists a line ℓ such that A ∈ ℓ ∧ B ∈ ℓ ∧ C ∈ ℓ. By the first axiom of order `collinear_of_between`, since A * B * C,  
-we prove that there exists a line ℓ such that A ∈ ℓ ∧ B ∈ ℓ ∧ C ∈ ℓ. Let this line be called `s`. Then, A ∈ s ∧ B ∈ s ∧ C ∈ s.
-**(ii)** Let us assume that A ≠ C. By contradiction, if A = C, then A * B * C would be equal to C * B * C. By the lemma `no_point_between_a_point`, this is 
-not possible, so we prove that A ≠ C.
-**(iii)** Let us assume that r = s. By the lemma `equal_lines_of_contain_two_points`, since A ≠ C, A ∈ r, A ∈ s, C ∈ r and C ∈ s, then we prove that
-r = s. Because r = s, then B ∈ s, which we proved in **(i)**, must be equivalent to B ∈ r. Therefore, the point B shares the same line `r` with the points
-A and C and satisfies that A * B * C.
-Hence, we have shown that a point that lies between two different collinear points shares the same line with them.
 -/
 
 /- Hint : Click here for a hint, in case you get stuck.
-Whenever we have a hypothesis of the form `h : P ∧ Q ∧ R`, we write `h.1` to refer to `P` and we type `h.2` to refer to `Q ∧ R`. If we want to refer to just 
-`Q`, we need to write `h.2.1`. Analogously, if we want to refer to just `R`, then we type `h.2.2`. 
-In case you don't know how to use the lemma `no_point_between_a_point`, look how you proved it in the previous level, so that you can adapt that code
-for this one.
+**Claim:** Given two different collinear points A and B, there is a third C that shares the same line with them and satisfies A * B * C.
+
+**Proof:** Let `r` be the line that is incident with the points A and B.
+
+**(i)** Let us assume that A ≠ B ∧ A ≠ C ∧ B ≠ C. By the first axiom of order `different_of_between`, since A * B * C, then we prove that 
+A ≠ B ∧ A ≠ C ∧ B ≠ C.
+ 
+**(ii)** Let us assume that ∃ ℓ, such that A ∈ ℓ ∧ B ∈ ℓ ∧ C ∈ ℓ. By the first axiom of order `collinear_of_between`, since A * B * C, then 
+we prove that ∃ ℓ, such that A ∈ ℓ ∧ B ∈ ℓ ∧ C ∈ ℓ. Let this line be called `s`. Then, A ∈ s ∧ B ∈ s ∧ C ∈ s.
+
+**(iii)** Let us assume that r = s. By the lemma `equal_lines_of_contain_two_points`, since A ≠ B, A ∈ r, A ∈ s, B ∈ r and B ∈ s, then we prove that
+r = s. Because C ∈ s, which we proved in **(ii)**, and r = s, then we prove that C ∈ r. 
+
+Hence, we have shown that given two different collinear points A and B, there is a third C that shares the same line with them and satisfies A * B * C.
+
 Still bewildered? Click on "View source" (located on the top right corner of the game screen) to see the solution.
 -/
 
@@ -35,7 +37,7 @@ variables {A B C P Q R : Ω} --hide
 variables {ℓ r s t : Line Ω} --hide
 
 /- Lemma :
-A point that lies between two different collinear points shares the same line with them.
+Given two different collinear points A and B, there is a third C that shares the same line with them and satisfies A * B * C.
 -/
 lemma between_points_share_line_v2 (hAr : A ∈ r) (hBr : B ∈ r) : 
 	(A * B * C) → C ∈ r :=
