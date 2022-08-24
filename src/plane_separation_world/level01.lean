3843600a23031886023c88962637ed43521c19e5
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-24 11:20:33 +0200

    Update level01.lean

--- src/plane_separation_world/level01.lean
+++ src/plane_separation_world/level01.lean
@@ -4,10 +4,30 @@ open IncidencePlane --hide
 /-
 # Plane Separation World
 
-## Level 1: ...
+## Level 1: a new world of possibilities...
 
-explanation
+The notion of **plane separation** comes from the fourth axiom of order, which is the Pasch's Axiom. 
 
+**B.4) Pasch's Axiom:** Let A, B, C be three non-collinear points and let ℓ be a line lying in the plane ABC
+and not passing through any of the points A, B, C. Then, if the line ℓ passes through a point of the segment A·B, 
+it will also pass through either a point of the segment B·C or a point of the segment A·C (but not both).
+
+Thanks to this, we can define what "being on the same side" means. 
+
+**Definition:** Given a line ℓ and the points A and B, such that A, B ∉ ℓ, we say that A and B are on the same side if
+the segment A·B does not meet ℓ or A = B.
+
+In Lean, the definition of `same_side` is represented as follows: 
+
+* `def same_side (ℓ : Line Ω) (P Q : Ω) :=  pts (P⬝Q) ∩ ℓ = ∅`
+
+The text `pts (P⬝Q) ∩ ℓ = ∅` can be read as "the intersection (**∩**) of the points in the segment P⬝Q and the line ℓ is an empty set (**∅**)". Therefore, 
+P and Q are on the same side of ℓ. 
+
+[**Rule of thumb:** Whenever you see `same_side` in Lean, use the `unfold` tactic. In this way, it will be easier to understand what it means. If it is 
+located at the hypothesis `h2`, for example, then `unfold same_side at h2,` will make progress. If it is located at the goal, then `unfold same_side,` will be enough 
+to rewrite the goal. This will change `same_side` into a text of the form `pts (P⬝Q) ∩ ℓ = ∅`. Then, you can use the `simp` tactic in the same way to change a text
+of the form `pts (P⬝Q) ∩ ℓ = ∅` into `{x : Ω | x = P ∨ x = Q ∨ P*x*Q} ∩ ↑ℓ = ∅`, which may feel more understandable to you.]
 -/
 
 /- Hint : Click here for a hint, in case you get stuck.
