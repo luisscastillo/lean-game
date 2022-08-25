e789a4c0f4db42f4d2d75888e0f09a584171585a
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-25 10:37:18 +0200

    Update level04.lean

--- src/plane_separation_world/level04.lean
+++ src/plane_separation_world/level04.lean
@@ -5,7 +5,6 @@ open IncidencePlane --hide
 # Plane Separation World
 
 ## Level 4: the Pasch's Axiom in action...
-
 To solve the following levels, we may want to use the lemma that we are going to prove now. Here you have some hints that could help you to step through it!
 **Hint 1:** Whenever you see the word `collinear`, the `unfold` tactic will make progress.
 **Hint 2:** Whenever you find a goal or hypothesis of the form `∀ {X : Ω}, X ∈ {A, B, C} → X ∈ r`, the `simp` tactic will make progress.
@@ -24,22 +23,13 @@ variables {ℓ r s t : Line Ω} --hide
 /- Lemma :
 Given three distinct points, they are on the same line if and only if they are collinear.
 -/
-lemma collinear_iff_on_line_through (h : A ≠ B) : collinear ({A, B, C} : set Ω) ↔ C ∈ line_through A B :=
+lemma exists_point_on_line (ℓ : Line Ω): ∃ A : Ω, A ∈ ℓ :=
 begin
-split,
-{
-  intro h1,
-  unfold collinear at h1,
-  cases h1 with ℓ hℓ,
-  simp at hℓ,
-  rw ← (incidence h hℓ.1 hℓ.2.1),
-  exact hℓ.2.2,
-},
-{
-  intro h1,
-  unfold collinear,
-  use line_through A B,
-  simp,
-  exact h1,
-},
+	have A2 := line_contains_two_points ℓ,
+  cases A2 with A hA,
+  cases hA with B hB,
+  cases hB with HAB hl,
+  use A,
+  rw hl,
+  exact line_through_left A B,
 end
