9101837b4d769edb58c0b2c84b22689c1a5e83db
Luis <107761623+luisscastillo@users.noreply.github.com>
2022-08-26 22:03:16 +0200

    Update level04.lean

--- src/plane_separation_world/level04.lean
+++ src/plane_separation_world/level04.lean
@@ -49,189 +49,102 @@ variables {ℓ r s t : Line Ω} --hide
 Given three non-collinear points A, B, C and a line ℓ that is not incident with them, if A and B are not on the same side of 
 ℓ and A and C are not on the same side of ℓ, then B and C are on the same side of ℓ.
 -/
-lemma at_most_two_classes_of_noncollinear (hA : A ∉ ℓ) (hB : B ∉ ℓ) (hC : C ∉ ℓ)
-    (hBneqC : B ≠ C) (hAB: ¬ same_side ℓ A B) (hAC: ¬ same_side ℓ A C)
-    (h : ¬ collinear ({A, B, C} : set Ω)) : same_side ℓ B C :=
+lemma same_side_trans_of_noncollinear (hCol : ¬ collinear ({A, C, B} : set Ω)):
+    same_side ℓ A B → same_side ℓ B C → same_side ℓ A C :=
 begin
-  --Step 1: First, prove that A is not equal to B and A is not equal to C
-  have HAB : A ≠ B,
+  have hAC : A ≠ C,
   {
-    intro H,
-    rw ← H at hAB,
-    apply hAB,
-    unfold same_side,
-    finish,
+    intro h,
+    apply hCol,
+    rw h,
+    use line_through B C,
+    simp, 
   },
-  have HAC : A ≠ C,
+-- Suppose l meets A·C (proving by contradiction)
+-- By Pasch l meets A·B or l meets B·C
+    -- If l meeets A·B then A, B on different sides
+    -- IF l meets B·C then B, C on different sides 
+  intros p q,
+  by_contradiction,
+  unfold same_side at h,
+  have h' : ∃ D , D ∈ pts (A⬝C) ∧ D ∈ ℓ,
   {
-    intro H,
-    rw ← H at hAC,
-    apply hAC,
-    unfold same_side,
-    finish,
-  },
---Step 1: Done
---Step 2: Prove that there exists a point D that is both on line ℓ and on line AB such that A*D*B
-  have HADB: ∃ (D : Ω), D ∈ ℓ ∧ D ∈ line_through A B ∧ A*D*B,
-  {
-    unfold same_side at hAB,
-    simp at hAB,
-    by_contra hc,
-    apply hAB,
-    push_neg at hc,
+    simp,
+    by_contra h',
+    push_neg at h',
+    apply h,
     ext,
     split,
     {
-      intro H,
-      exfalso,
-      have h1: x ∈ line_through A B,
+      intro h'',
+      simp,
+      specialize h' x,
+      have h''' : A*x*C,
       {
-        cases H with H hxl,
-        simp  at H,
-        cases H,
-          rw H,
-          exact line_through_left A B,
-        cases H,
-          rw H,
-          exact line_through_right A B,
-        rw ← collinear_iff_on_line_through HAB,
-        have H1:= collinear_of_between H,
-        cases H1 with r hr,
-        use r,
-        simp,
+        finish,
+      },
+      apply h',
+      {
         tauto,
       },
-      specialize hc x H.2,
-      finish,
+      {
+        finish,
+      },  
     },
-    tauto,
-    },
-    -- D POINT CREATED
-  -- Repeat the same process for another point E that should lie on line ℓ and line AC
-  have HAEC: ∃ (E : Ω), E ∈ ℓ ∧ E ∈ line_through A C ∧ A*E*C,
-  {
-    unfold same_side at hAC,
-    simp at hAC,
-    by_contra hc,
-    apply hAC,
-    push_neg at hc,
-    ext,
-    split,
     {
-      intro H,
-      exfalso,
-      have h1: x ∈ line_through A C,
-      {
-        cases H with H hxl,
-        simp  at H,
-        cases H,
-          rw H,
-          exact line_through_left A C,
-        cases H,
-          rw H,
-          exact line_through_right A C,
-        rw ← collinear_iff_on_line_through HAC,
-        have H1:= collinear_of_between H,
-        cases H1 with r hr,
-        use r,
-        simp,
-        tauto,
-      },
-      specialize hc x H.2,
-      finish,
+      tauto,
     },
-    tauto,
-    },
+},
 
-  --BOTH E AND D POINTS HAVE BEEN PROVED
+cases h' with P r,
+have c : A ∉ ℓ ∧ B ∉ ℓ ∧ C ∉ ℓ,
+{
+  repeat {split},
+  apply not_in_line_of_same_side_left p,
+  apply not_in_line_of_same_side_right p,
+  apply not_in_line_of_same_side_right q,
+},
+simp at r,
+have x : A*P*C,
+{
+  have hPA : P ≠ A,
+  {
+    intro hc,
+    apply c.1,
+    rw hc at r,
+    exact r.2,
+  },
+  have hPA : P ≠ C,
+  {
+    intro hcon,
+    apply c.2.2,
+    rw hcon at r,
+    exact r.2,
+  },
+  tauto,
+},
+have a1 : B ∉ line_through A C,
+{
+  rw ← collinear_iff_on_line_through hAC,
+  exact hCol,
+},
 
-  -- WE NEED TO SHOW THAT C IS NOT IN BETWEEN A and B
-  -- USE LEMMA (not_on_line_iff_not_collinear) STATING THAT IF POINTS A B C ARE NOT COLLINEAR AND A≠B,  THEY CANNOT BE IN THE SAME LINE
-  cases HADB with D r,
-  cases r with l1 r1,
-  cases r1 with l2 r2,
-      {
-        --PASCH AXIOM
-        --MAKE PASCH A HYPOTHESIS AND THEN SOLVE THE TWO CASES
-        have w := pasch (not_on_line_iff_not_collinear HAB h) hA hB hC l1 r2,
-        unfold same_side,
-        cases w,
-        {
-        --FIRST PASCH CASE 
-          cases w,
-          {
-              simp,
-              ext,
-              split,
-              {
-                intro H,
-                exfalso,
-                have x1: x ∈ line_through B C,
-                {
-                    cases H with H hxl,
-                    simp at H,
-                    cases H,
-                      rw H,
-                      exact line_through_left B C,
-                    cases H,
-                      rw H,
-                      exact line_through_right B C,
-                    rw ← collinear_iff_on_line_through hBneqC,
-                    have H1:= collinear_of_between H,
-                    cases H1 with r0 hr,
-                    use r0,
-                    simp,
-                    tauto,
-                },
-                simp at H,
-                cases H,
-                finish,
-              },
-              {
-                intro H,
-                exfalso,
-                simp at H,
-                cases H,
-              },
-          }, 
-        },
-        {
-          --SECOND PASCH CASE
-          simp,
-              ext,
-              split,
-              {
-                intro H,
-                exfalso,
-                have x1: x∈ line_through B C,
-                {
-                    cases H with H hxl,
-                    simp at H,
-                    cases H,
-                      rw H,
-                      exact line_through_left B C,
-                    cases H,
-                      rw H,
-                      exact line_through_right B C,
-                    rw ← collinear_iff_on_line_through hBneqC,
-                    have H1:= collinear_of_between H,
-                    cases H1 with r0 hr,
-                    use r0,
-                    simp,
-                    tauto,
-                },
-                simp at H,
-                cases H,
-                finish,
-              },
-              {
-                intro H,
-                exfalso,
-                simp at H,
-                cases H,
-              },
-          },
-        },
-  -- BOTH PASCH CASES SOLVED AND PROOF COMPLETED
+have fact := pasch a1 c.1 c.2.2 c.2.1 r.2 x,
+cases fact,
+{
+unfold same_side at p,
+cases fact.1 with E,
+simp [set.eq_empty_iff_forall_not_mem] at p,
+replace p := p.2.2 E h_1.2,
+apply p,
+exact h_1.1,
+},
+unfold same_side at q,
+cases fact.1 with F,
+simp [set.eq_empty_iff_forall_not_mem] at q,
+rw between_symmetric at h_1,
+replace q := q.2.2 F h_1.2,
+apply q,
+exact h_1.1,
 end
 
